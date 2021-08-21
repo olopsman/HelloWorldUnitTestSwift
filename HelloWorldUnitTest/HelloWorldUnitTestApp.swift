d9d4530f5192779e1f73779658205e10e798849b
Paulo Orquillo <paulo.orquillo@gmail.com>
2021-08-21 17:44:59 +1200

    Initial Commit

---
+++ HelloWorldUnitTest/HelloWorldUnitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldUnitTestApp.swift
+//  HelloWorldUnitTest
+//
+//  Created by Paulo Orquillo on 21/08/21.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldUnitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
